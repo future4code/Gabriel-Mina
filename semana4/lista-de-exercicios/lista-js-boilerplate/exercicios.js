bd9fe3454c1167d4d23dfb347f72dcf1225253c7
gabrielMina <gabrielmina118@gmail.com>
2021-03-05 17:36:14 -0300

    subindo projeto 7

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -84,6 +84,18 @@ function retornaExpressoesBooleanas() {
 
 function retornaNNumerosPares(n) {
    // implemente sua lógica aqui
+   const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+   const arrayPares = [];
+   const arrayRetorno = [];
+   array.forEach((pares) =>{
+      if(pares % 2 == 0){
+         arrayPares.push(pares);
+      }
+   }) 
+   for(let i = 0 ; i < n ; i++){
+      arrayRetorno.push(arrayPares[i]);
+   }
+   return arrayRetorno; 
 }
 
 // Exercício 8
