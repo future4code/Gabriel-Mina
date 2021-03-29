8659cc8e6b4c9bde3877db0117eefa95489d9003
gabrielMina <gabrielmina118@gmail.com>
2021-03-29 08:47:59 -0300

    subindo desafio

--- semana7/labefy/labefy2.0/src/Components/PlayList/PlayList.js
+++ semana7/labefy/labefy2.0/src/Components/PlayList/PlayList.js
@@ -1,11 +1,13 @@
 import react from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+
 const Principal = styled.div`
     height:100vh;
     width:100%;
     display:flex;
-    justify-content:center;
+    flex-direction:row;
+    justify-content:space-evenly;
     align-items:center;
 `
 const Playlistas = styled.div`
@@ -29,50 +31,141 @@ const BotaoPlayLista = styled.button`
 const ListaMusicas = styled.ol`
     font-size:30px;
 `
+const TituloDivFilha = styled.h3`
+    font-weight:bold;
+    text-transform:uppercase;
+    display:flex;
+    justify-content:center;
+    flex-direction:column;
+    align-items:center;
+`
+const PrincipalDivFilha = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:space-evenly;
+    height:300px;
+    text-align:center;
+    @media(max-width:800px){
+        height:20vh;
+    }
+`
+
 export default class PlayList extends react.Component {
 
     state = {
-        lista: []
+        lista: [],
+        detalhes: [],
+        valorNome: "",
+        valorArtista: "",
+        valorUrl: "",
+        inserirMusica:""
     }
-    
-    componentDidMount(){
+
+    alteraNome = (e) => {
+        this.setState({ valorNome: e.target.value })
+    }
+
+    alteraArtista = (e) => {
+        this.setState({ valorArtista: e.target.value })
+    }
+
+    alteraUrl = (e) => {
+        this.setState({ valorUrl: e.target.value })
+    }
+
+    componentDidMount() {
         this.listarPlayList();
     }
 
-    listarPlayList = async() =>{
-        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",{
-            headers:{
-                Authorization:"gabriel-mina-cruz"
+    listarPlayList = () => {
+        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists", {
+            headers: {
+                Authorization: "gabriel-mina-cruz"
             }
-        }).then((resposta) =>{
-            this.setState({lista:resposta.data.result.list})
-        }).catch((erro)=>{
+        }).then((resposta) => {
+            this.setState({ lista: resposta.data.result.list })
+        }).catch((erro) => {
             console.log(erro.resposta.data);
         })
     }
- 
-    deletarPlayList = (id) =>{
-        if(window.confirm("deseja realmente excluir")){
-            axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`,{
-                headers:{
-                    Authorization:"gabriel-mina-cruz"
+
+    deletarPlayList = (id) => {
+        if (window.confirm("deseja realmente excluir")) {
+            axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`, {
+                headers: {
+                    Authorization: "gabriel-mina-cruz"
                 }
-            }).then((resposta)=>{
+            }).then((resposta) => {
                 this.listarPlayList();
                 console.log(resposta.data);
-            }).catch((erro)=>{
+            }).catch((erro) => {
                 console.log(erro.resposta.data);
             })
         }
     }
 
-    render() {
-        const listarNaTela = this.state.lista.map((musicas)=>{
-            return <li key={musicas.id}>
-                    {musicas.name}
-                    <BotaoPlayLista onClick={()=> this.deletarPlayList(musicas.id)}>Deletar</BotaoPlayLista>
-                   </li>
+    detalhesPlayList = (id) => {
+        axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`, {
+            headers: {
+                Authorization: "gabriel-mina-cruz"
+            }
+        }).then((resposta) => {
+            console.log("respostaData", resposta.data);
+            this.setState({ detalhes: resposta.data.result.tracks })
+            console.log("state", this.state.detalhes);
+        }).catch((erro) => {
+            console.log("errooo", erro);
         })
+    }
+
+    inserirMusica = (id) => {
+        const body = {
+            "name": this.state.valorNome,
+            "artist": this.state.valorArtista,
+            "url": this.state.valorUrl
+        }
+        axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`, body, {
+            headers: {
+                Authorization: "gabriel-mina-cruz"
+            }
+        }).then((resposta) => {
+            console.log("respostaData", resposta.data);
+            alert("Musica inserida com sucesso");
+        }).catch((erro) => {
+            console.log(erro.resposta.data);
+        })
+    }
+
+    inserirMusicaForm = (e) =>{
+        this.setState({inserirMusica: e.target.value})
+    }
+
+    render() {
+        const listarNaTela = this.state.lista.map((musicas) => {
+            return <li key={musicas.id}>
+                {musicas.name}
+                <BotaoPlayLista onClick={() => this.deletarPlayList(musicas.id)}>Deletar</BotaoPlayLista>
+                <BotaoPlayLista onClick={() => this.detalhesPlayList(musicas.id)}>Detalhes</BotaoPlayLista>
+            </li>
+        })
+
+        const dorpdonw = this.state.lista.map((musicas) => {
+            return <option
+                value={musicas.name}
+            >
+                {musicas.name}
+            </option>
+
+        })
+
+        const listaDetalhes = this.state.detalhes.map((musicas) => {
+            return <div key={musicas.id}>
+                <p>Nome: {musicas.name}</p>
+                <p>Artista: {musicas.artist}</p>
+                <p>Url: {musicas.url}</p>
+            </div>
+        })
+
         return (
             <div>
                 <Principal>
@@ -81,7 +174,26 @@ export default class PlayList extends react.Component {
                         <ListaMusicas>
                             {listarNaTela}
                         </ListaMusicas>
+                        {listaDetalhes}
                     </Playlistas>
+                    <TituloDivFilha>
+                        <PrincipalDivFilha>
+                            <p>Inserir Musica</p>
+                            <label>Nome:</label>
+                            <input type="text" value={this.state.valorNome} onChange={this.alteraNome} />
+                            <label>Artista:</label>
+                            <input type="text" value={this.state.valorArtista} onChange={this.alteraArtista} />
+                            <label>Url clip:</label>
+                            <input type="text" value={this.state.valorUrl} onChange={this.alteraUrl} />
+                            <label>Insira qual playList</label>
+                            <form>
+                                <select value={this.state.inserirMusica} onChange={this.inserirMusicaForm}>
+                                    {dorpdonw}
+                                </select><br></br>
+                                <input type="submit" value="Enviar" />
+                            </form>
+                        </PrincipalDivFilha>
+                    </TituloDivFilha>
                 </Principal>
             </div>
         )
