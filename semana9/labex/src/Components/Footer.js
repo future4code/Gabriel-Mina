88e43d76340ec29fec7f9eecc9a3131dbbdc86cc
gabrielMina <gabrielmina118@gmail.com>
2021-04-13 10:57:59 -0300

    aplicando estilizacao na main

---
+++ semana9/labex/src/Components/Footer.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+const Principal = styled.div`
+    display:flex;
+    flex:1;
+    flex-direction:column;
+    background-color:#000000;
+    color:#ffffff;
+`
+const ItensFooter = styled.div`
+    width:200px;
+    display:flex;
+    text-align:center;
+    flex-direction:column;
+`
+const TituloItensFooter = styled.h2`
+    text-transform:uppercase;
+`
+const ItensPrincipalFooter = styled.div`
+    display:flex;
+    justify-content:space-around;
+    border:1px solid white;
+`
+const RedesSociais = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
+export default function Footer() {
+    return (
+        <Principal>
+            <ItensPrincipalFooter>
+                <ItensFooter>
+                    <TituloItensFooter>Sobre</TituloItensFooter>
+                    <h4>Como Funciona</h4>
+                    <h4>Investidores</h4>
+                    <h4>Carreiras</h4>
+                </ItensFooter>
+                <ItensFooter>
+                    <TituloItensFooter>Hospedagem</TituloItensFooter>
+                    <h4>Lugares para ficar</h4>
+                    <h4>Todos os Destinos</h4>
+                    <h4>Avaliações</h4>
+                </ItensFooter>
+                <ItensFooter>
+                    <TituloItensFooter>Região</TituloItensFooter>
+                    <h4>Plutão</h4>
+                    <h4>Vênus</h4>
+                    <h4>Marte</h4>
+                </ItensFooter>
+                <ItensFooter>
+                    <TituloItensFooter>Atendimento</TituloItensFooter>
+                    <h4>Central de Ajuda</h4>
+                    <h4>Canal de apoio</h4>
+                    <h4>Opção de Cancelamento</h4>
+                </ItensFooter>
+            </ItensPrincipalFooter>
+            <RedesSociais>
+                <p>Lorem ipsium Lorem ipsium Lorem ipsium Lorem ipsium Lorem ipsium</p>
+                <p>LABEX</p>
+                <p>RedesSociais</p>
+                <p>&copy; Todos os direitos autorais para Labex- LTDA</p>
+            </RedesSociais>
+        </Principal>
+    )
+}
---
+++ semana9/labex/src/Components/Header.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import logo from '../img/x.png';
+/*
+@media (max-width: 800px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+*/
+const Principal = styled.div`
+    display:flex;
+    flex:1;
+    align-items:center;
+    font-size:50px;
+    width:100vw;
+    background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), #000000;
+    color:#ffffff;
+    justify-content:space-between;
+`
+const Logo = styled.div`
+    display:flex;
+    align-items:center;
+    margin-left:4px;
+`
+const ImagemLogo = styled.img`
+    width:100px;
+    height:100px;
+    background-color:#ffffff;
+    border:2px solid black;
+`
+
+export default function Header(){
+    return(
+        <Principal>
+            <Logo>
+                <ImagemLogo src={logo} />
+                <h2>LABEX</h2>
+            </Logo>
+            <div>Area Do Admin</div>
+        </Principal>
+    )
+}
