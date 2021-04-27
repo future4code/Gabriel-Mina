1e6dd2da5aeedaa686a90d1f4c1d303b81cc448b
gabrielMina <gabrielmina118@gmail.com>
2021-04-27 08:33:29 -0300

    fazendo tela de cadastro

--- semana11/Labeditt/labeddit/src/Pages/SingUp/SingUp.js
+++ semana11/Labeditt/labeddit/src/Pages/SingUp/SingUp.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { ScreenContainer, LogoImagem} from './SingUpStyle'
+import logo from '../../Assets/reditSignUp.jpg'
+import SingUpForm from './SingUpForm'
 
-const SingUp = () =>{
-    return(
-        <h1>SingUp</h1>
+
+const SingUp = () => {
+    return (
+        <ScreenContainer>
+            <LogoImagem src={logo} />
+            <SingUpForm />
+        </ScreenContainer>
     )
 }
 export default SingUp;
