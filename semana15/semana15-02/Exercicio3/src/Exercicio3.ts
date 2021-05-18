6ca7ae909ce8739d20c62df95ba9a5bea2ad02e5
gabrielMina <gabrielmina118@gmail.com>
2021-05-18 16:17:47 -0300

    exercicios typescript

---
+++ semana15/semana15-02/Exercicio3/src/Exercicio3.ts
@@ -0,0 +1,38 @@
+// A entrada da função é um array de post e um nome em String e retorna um array
+type post = {
+    autor: string,
+    texto: string
+}
+
+
+const posts:post[] = [
+    {
+        autor: "Alvo Dumbledore",
+        texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+        autor: "Severo Snape",
+        texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+        autor: "Hermione Granger",
+        texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+        autor: "Dobby",
+        texto: "Dobby é um elfo livre!"
+    },
+    {
+        autor: "Lord Voldemort",
+        texto: "Avada Kedavra!"
+    }
+]
+
+function buscarPostsPorAutor(posts:post[], autorInformado:string) {
+    return posts.filter(
+      (post) => {
+        return post.autor.includes(autorInformado) 
+      }
+    )
+  }
+console.log(buscarPostsPorAutor(posts,"D"));
